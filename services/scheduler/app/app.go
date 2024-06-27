b02f23045a1b819605270acfc2115549621afadb
bkrasnopolska <bkrasnopolska@objectivity.co.uk>
2024-06-27 13:05:03 +0200

    add logic to fr=orwardListCollection and service in docker compose

--- services/scheduler/app/app.go
+++ services/scheduler/app/app.go
@@ -1,13 +1,19 @@
 package app
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "github.com/google/uuid"
-  swaggerfiles "github.com/swaggo/files"
-  ginSwagger "github.com/swaggo/gin-swagger"
-  docs "github.com/MichalK12344321/golang/services/scheduler/docs"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	docs "github.com/MichalK12344321/golang/services/scheduler/docs"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
 // /trigger POST
 // /collections GET
 // /{collectionId}/log GET
@@ -62,6 +68,38 @@ func forwardListCollectionsHandler(c *gin.Context)  {
 	// get config for Collection Data Gateway URL and PORT
 	// Forward GET request to Collection Data Gateway
 	// handle response
+
+  collectionDataGatewayHost := os.Getenv("COLLECTION_DATA_GATEWAY_HOST")
+  if collectionDataGatewayHost == "" {
+    collectionDataGatewayHost = "collectiondatagateway"
+}
+
+  collectionDataGatewayPort := os.Getenv("COLLECTION_DATA_GATEWAY_PORT")
+  if collectionDataGatewayPort == "" {
+    collectionDataGatewayPort = "5555"
+}
+  collectionDataGatewayURL := fmt.Sprintf("http://%s:%s/api/v1/collections", collectionDataGatewayHost, collectionDataGatewayPort)
+
+  resp, err := http.Get(collectionDataGatewayURL)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error":"Failed to read response from Get request Collection Data Gateway"})
+    return
+  }
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error":"Failed to read response from response body Collection Data Gateway"})
+    return
+  }
+
+  var collections CollectionsType
+  if err := json.Unmarshal(body, &collections); err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse response from Collection Data Gateway"})
+    return
+  }
+
+  c.JSON(http.StatusOK, collections)
 }
 
 type ConnectionInfoType struct {
